a386ec96e1c4e14392786915a3b82f6881902680
belleua <maua5002@gmail.com>
2026-06-07 23:03:20 -0400

    Delete github/workflows/index.js

--- github/workflows/index.js
+++
@@ -1,6 +0,0 @@
-// Hola Mundo - ITLA 2026
-// Practica: Integracion Continua con GitHub Actions
-
-console.log("¡Hola Mundo!");
-console.log("Repositorio desplegado exitosamente.");
-console.log(`Fecha: ${new Date().toLocaleString("es-DO")}`);
