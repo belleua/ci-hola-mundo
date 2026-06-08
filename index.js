ef294c136b413409c43a83d021708072f99d72b1
belleua <maua5002@gmail.com>
2026-06-07 23:04:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Hola Mundo - ITLA 2026
+// Practica: Integracion Continua con GitHub Actions
+
+console.log("¡Hola Mundo!");
+console.log("Repositorio desplegado exitosamente.");
+console.log(`Fecha: ${new Date().toLocaleString("es-DO")}`);
